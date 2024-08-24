fde7783890c8c177ba9a66cf5c59eaef22c232e6
Artxe Web Diseño web Ponferrada SEO <117656534+artxewebponferrada@users.noreply.github.com>
2024-08-25 00:12:46 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,18 +6,15 @@ export function middleware(request: NextRequest) {
   const basicAuth = request.headers.get('authorization');
 
   if (basicAuth) {
-    // Decodificar las credenciales de Basic Auth usando Buffer
+    // Decodificar las credenciales usando Buffer
     const authValue = basicAuth.split(' ')[1];
-    const decodedValue = Buffer.from(authValue, 'base64').toString('ascii');
+    const decodedValue = Buffer.from(authValue, 'base64').toString('utf-8');
     const [user, password] = decodedValue.split(':');
 
-    // Log para depuración
-    console.log('Decoded credentials:', decodedValue);
-
-    // Verificación simple con credenciales hardcoded
+    // Verificar credenciales hardcoded
     if (user === 'admin' && password === 'admin') {
       console.log('Authentication successful');
-      return NextResponse.next(); // Autenticación exitosa, continuar
+      return NextResponse.next(); // Continuar si la autenticación es exitosa
     } else {
       console.log('Authentication failed');
     }
@@ -25,7 +22,7 @@ export function middleware(request: NextRequest) {
     console.log('Authorization header not found');
   }
 
-  // Si la autenticación falla, solicitar credenciales nuevamente
+  // Responder con un 401 si la autenticación falla
   return new NextResponse('Authentication required', {
     status: 401,
     headers: {
