d9e696887e0f26bff5f4c59d260af910d85b541d
Artxe Web Diseño web Ponferrada SEO <117656534+artxewebponferrada@users.noreply.github.com>
2024-08-25 00:17:26 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,27 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { appConfig } from "@/config";
+import createMiddleware from "next-intl/middleware";
 
-export function middleware(request: NextRequest) {
-  // Obtener el encabezado de autorización
-  const basicAuth = request.headers.get('authorization');
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
 
   if (basicAuth) {
-    // Decodificar las credenciales usando Buffer
     const authValue = basicAuth.split(' ')[1];
     const decodedValue = Buffer.from(authValue, 'base64').toString('utf-8');
     const [user, password] = decodedValue.split(':');
 
-    // Verificar credenciales hardcoded
     if (user === 'admin' && password === 'admin') {
-      console.log('Authentication successful');
-      return NextResponse.next(); // Continuar si la autenticación es exitosa
+      // Continuar con el middleware de internacionalización después de la autenticación exitosa
+      const intlMiddleware = createMiddleware({
+        locales: appConfig.i18n.locales,
+        defaultLocale: appConfig.i18n.defaultLocale,
+        localePrefix: "as-needed",
+        localeDetection: true,
+        alternateLinks: true
+      });
+
+      return intlMiddleware(req); // No es necesario modificar la solicitud
     } else {
       console.log('Authentication failed');
     }
@@ -22,7 +29,7 @@ export function middleware(request: NextRequest) {
     console.log('Authorization header not found');
   }
 
-  // Responder con un 401 si la autenticación falla
+  // Solicitar autenticación si no es exitosa
   return new NextResponse('Authentication required', {
     status: 401,
     headers: {
