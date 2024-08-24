a60af2095192b07d19ff7dbd354e4c3eeed04443
Artxe Web Diseño web Ponferrada SEO <117656534+artxewebponferrada@users.noreply.github.com>
2024-08-24 23:31:35 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,54 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
 import createMiddleware from "next-intl/middleware";
+import { NextRequest } from "next/server";
 import { appConfig } from "./config";
-
-// Función para manejar la autenticación básica
-function basicAuth(req: NextRequest) {
-  const authHeader = req.headers.get('authorization');
-
-  if (!authHeader) {
-    return new NextResponse('Auth required', {
-      status: 401,
-      headers: {
-        'WWW-Authenticate': 'Basic realm="Secure Area"'
-      }
-    });
-  }
-
-  const base64Credentials = authHeader.split(' ')[1];
-  const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-  const [username, password] = credentials.split(':');
-
-  // Verifica las credenciales
-  const isAuthorized = username === 'admin' && password === 'supersecret';
-
-  if (!isAuthorized) {
-    return new NextResponse('Invalid credentials', {
-      status: 401,
-      headers: {
-        'WWW-Authenticate': 'Basic realm="Secure Area"'
-      }
-    });
-  }
-
-  return null; // Autenticación exitosa
-}
 
 export default async function middleware(req: NextRequest) {
 
-  // Verifica la autenticación
-  const authResult = basicAuth(req);
-  if (authResult) {
-    return authResult;
-  }
-
-  // Procede con el middleware de internacionalización si la autenticación es exitosa
   const intlMiddleware = createMiddleware({
     locales: appConfig.i18n.locales,
     defaultLocale: appConfig.i18n.defaultLocale,
     localePrefix: "as-needed",
     localeDetection: false,
-    alternateLinks: true,
+    alternateLinks: true
   });
 
   return intlMiddleware(req);
