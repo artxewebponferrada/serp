a747d516a7d39324b1c7a76c0cf7d9d45ae21311
Artxe Web Diseño web Ponferrada SEO <117656534+artxewebponferrada@users.noreply.github.com>
2024-08-25 00:07:00 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,31 +2,30 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  // Obtener el encabezado de autorización
   const basicAuth = request.headers.get('authorization');
 
   if (basicAuth) {
+    // Decodificar las credenciales de Basic Auth usando Buffer
     const authValue = basicAuth.split(' ')[1];
-    const decodedValue = atob(authValue);
-
-    console.log('Authorization Header:', basicAuth);
-    console.log('Decoded Credentials:', decodedValue);
-
+    const decodedValue = Buffer.from(authValue, 'base64').toString('ascii');
     const [user, password] = decodedValue.split(':');
 
-    console.log('User:', user);
-    console.log('Password:', password);
+    // Log para depuración
+    console.log('Decoded credentials:', decodedValue);
 
-    const validUser = 'admin';
-    const validPassword = 'supersecret';
-
-    if (user === validUser && password === validPassword) {
+    // Verificación simple con credenciales hardcoded
+    if (user === 'admin' && password === 'admin') {
       console.log('Authentication successful');
-      return NextResponse.next();
+      return NextResponse.next(); // Autenticación exitosa, continuar
     } else {
       console.log('Authentication failed');
     }
+  } else {
+    console.log('Authorization header not found');
   }
 
+  // Si la autenticación falla, solicitar credenciales nuevamente
   return new NextResponse('Authentication required', {
     status: 401,
     headers: {
