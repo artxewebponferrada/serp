882c91a55183c914fc71abe7b57cedb747aa2422
Artxe Web Diseño web Ponferrada SEO <117656534+artxewebponferrada@users.noreply.github.com>
2024-08-24 23:39:06 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-import createMiddleware from "next-intl/middleware";
 import { NextRequest, NextResponse } from "next/server";
 import { appConfig } from "./config";
 
-export default async function middleware(req: NextRequest) {
-  // Verifica la autenticación básica
+// Middleware principal
+export default function middleware(req: NextRequest) {
   const authHeader = req.headers.get('authorization');
 
+  // Si no hay encabezado de autorización, pedir credenciales
   if (!authHeader) {
     return new NextResponse('Auth required', {
       status: 401,
@@ -19,7 +19,7 @@ export default async function middleware(req: NextRequest) {
   const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
   const [username, password] = credentials.split(':');
 
-  // Comprueba las credenciales
+  // Verifica las credenciales
   if (username !== 'admin' || password !== 'supersecret') {
     return new NextResponse('Invalid credentials', {
       status: 401,
@@ -29,16 +29,8 @@ export default async function middleware(req: NextRequest) {
     });
   }
 
-  // Si la autenticación es exitosa, continúa con el middleware de internacionalización
-  const intlMiddleware = createMiddleware({
-    locales: appConfig.i18n.locales,
-    defaultLocale: appConfig.i18n.defaultLocale,
-    localePrefix: "as-needed",
-    localeDetection: false,
-    alternateLinks: true
-  });
-
-  return intlMiddleware(req);
+  // Si las credenciales son correctas, continúa con la respuesta normal
+  return NextResponse.next();
 }
 
 export const config = {
